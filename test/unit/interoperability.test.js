a2993719f0b1cf52cacd9a6d37d19747ce70a69c
Sega NDIAYE <sega.ndiaye@cncc.fr>
2021-04-30 22:25:11 +0200

    fix unit tests errors

--- test/unit/interoperability.test.js
+++ test/unit/interoperability.test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const driverbuilder = require('../../lib/driverbuilder');
-const helpers = require('../../lib/helpers');
 const main = require('../../lib/main');
 
 describe('interoperability should be ok', () => {
@@ -9,10 +8,7 @@ describe('interoperability should be ok', () => {
     test('has interoperability', () => {
         expect(driverbuilder).toHaveProperty('build');
 
-        expect(helpers).toHaveProperty('driver');
-        expect(helpers).toHaveProperty('start');
-
-        expect(main).toHaveProperty('chooseBrowser');
+        expect(main).toHaveProperty('chooseUrlAndBrowser');
         expect(main).toHaveProperty('takeScreenshot');
     });
 
